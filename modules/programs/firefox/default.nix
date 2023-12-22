1ed2741e6688a2268b5999bbf962144d438a0b00
Vonixxx <vonixxxwork@tuta.io>
2023-12-22 11:36:21 +0100

    updated

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -46,10 +46,10 @@
              enable = true;
              "2810"."privacy.sanitize.sanitizeOnShutdown".value = true;
              "2811" = {
-               "privacy.clearOnShutdown.formdata".value     = true;
+               "privacy.clearOnShutdown.cache".value        = true;
                "privacy.clearOnShutdown.downloads".value    = true;
-               "privacy.clearOnShutdown.cache".value        = false;
                "privacy.clearOnShutdown.history".value      = false;
+               "privacy.clearOnShutdown.formdata".value     = false;
                "privacy.clearOnShutdown.sessions".value     = false;
                "privacy.clearOnShutdown.siteSettings".value = false;
              };
@@ -63,16 +63,17 @@
                "privacy.resistFingerprinting.pbmode".value = false;              
              };
              "4504"."privacy.resistFingerprinting.letterboxing".value = false;
+             "4520"."webgl.disabled".value                            = false;
            };
 
-           "5000".enable = true;
+           "5000".enable = false;
            "5500".enable = false;
 
-           "6000".enable = true;
+           "6000".enable = false;
 
-           "7000".enable = true;
+           "7000".enable = false;
 
-           "8000".enable = true;
+           "8000".enable = false;
 
            "9000".enable = true;
          };
